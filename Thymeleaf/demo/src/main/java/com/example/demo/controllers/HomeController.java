be37a43999a14aa6709c7426ce6dded3d92cf0e7
Exmator <151688577+Exmator@users.noreply.github.com>
2025-08-04 20:46:17 -0500

    ADD: Groceries page
    A groceries page was added. This page uses an Array list that is iterated using a for loop in html

--- Thymeleaf/demo/src/main/java/com/example/demo/controllers/HomeController.java
+++ Thymeleaf/demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controllers;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,5 +12,18 @@ public class HomeController {
     @GetMapping("/")
     public String HOME() {
         return "index";
+    }
+    @GetMapping("/groceries")
+    public String GROCERIES(Model model) {
+        model.addAttribute("features", Arrays.asList(
+            "15 eggs",
+            "2 liters of milk",
+            "1 loaf of bread",
+            "500 grams of cheese",
+            "1 kg of apples",
+            "5 kg of bananas",
+            "1 kg of oranges"
+        ));
+        return "groceries";
     }
 }
